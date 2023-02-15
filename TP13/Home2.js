1ce30539a5d0b54d8814591f21d26bfa21c5d52a
Emanuel Gomez <113939325+olkothh@users.noreply.github.com>
2023-02-15 15:50:53 -0300

    Add files via upload

---
+++ TP13/Home2.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Menu from './Menu';
+
+function Home() {
+  return (
+    <div>
+      <Menu />
+      <h1>Esta es mi primer APP React</h1>
+    </div>
+  );
+}
+
+export default Home;
